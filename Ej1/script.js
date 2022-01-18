32d86abd668558e0264f1221e9bd6916b98018d3
Gerardo096 <gerardopsb96@gmail.com>
2022-01-18 15:34:39 +0100

    "efecto" hecho

--- Ej1/script.js
+++ Ej1/script.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
     $("p").hover(function() {
 
+
         $(this).css("color", "white");
         $(this).animate({ right: '60px' });
+
     }, function() {
-        $(this).animate({ left: '60px' });
+
+        $(this).animate({ right: '60px' });
         $(this).css("color", "black");
     });
 });
