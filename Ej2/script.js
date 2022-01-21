4a3c60e96711cef426b0af5413965e76eb16938a
Gerardo096 <gerardopsb96@gmail.com>
2022-01-21 16:54:48 +0100

    ej 2 hecho

---
+++ Ej2/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+    let cont = 0;
+    $("#insertar").click(function(event) {
+        var timestamp = event.timeStamp;
+        if (cont == 0 || cont % 7 == 0) {
+
+            $("table").append(" <tr><td><img src='https://loremflickr.com/200/200?random+" + timestamp + "'></td></tr>");
+        } else {
+            $("table tr").append(" <td><img src='https://loremflickr.com/200/200?random+" + timestamp + "'></td>");
+            $("table tr td").css("display", "none");
+            $("table tr td").fadeIn(1000);
+
+
+        }
+        cont++;
+    });
+    $("#eliminar").click(function(event) {
+        $("table tr td").last().remove();
+
+    });
+});
